4d3a496bcf26041d0241d961b4641861842061f5
Angel Sánchez <anghelo@gmail.com>
2024-02-02 18:31:31 -0600

    Version inicial

---
+++ src/main/java/com/ats/reservasrestaurante/ReservasrestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.ats.reservasrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasrestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasrestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ats/reservasrestaurante/ReservasrestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.reservasrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasrestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
